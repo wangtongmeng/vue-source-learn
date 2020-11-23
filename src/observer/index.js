c65c15fad2dd54f4a44204304d98f0f46ed9c80a
wangtongmeng <tongmengwang@163.com>
2020-11-24 06:08:46 +0800

    Vue响应式原理：递归属性劫持（对象）
    对对象的属性进行劫持
    
    - 1.定义Observer类，观测数据
    - 2.defineReactive定义响应式数据
    - 3.如果对象属性值还是对象则递归深度监测
    - 4.当对data进行赋值操作，且值是对象时，也需要观测数据

--- src/observer/index.js
+++ src/observer/index.js
@@ -1,3 +1,39 @@
+import {
+    isObject
+} from '../util/index'
+
+class Observer {
+    constructor (value) {
+        // vue 如果数据的层次过多 需要递归的解析对象中的属性，依次增加set和get方法
+        this.walk(value)
+    }
+    walk(data) {
+        let keys = Object.keys(data)
+        keys.forEach(key => {
+            defineReactive(data, key, data[key]) // 定义响应式数据
+        })
+    }
+}
+
+function defineReactive(data, key, value) {
+    observe(value) // 递归实现深度监测（数据越深，递归越多，从而导致性能浪费，所以写代码时，层级不要太多）
+    Object.defineProperty(data, key, {
+        get() { // 获取值时做一些操作
+            return value
+        },
+        set(newValue){ // 设置值时也可以做一些操作
+            if (newValue === value) return
+            observe(newValue) // 继续劫持用户设置的值，因为有可能用户设置的值是一个对象
+            value = newValue
+        }
+    })
+    
+}
+
 export function observe(data) {
-    console.log(data)
+    let isObj = isObject(data)
+    if (!isObj) return
+    
+    return new Observer(data) // 用来观测数据
+
 }
