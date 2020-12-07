dc45fe9185208fce3718512064f89e215de55768
wangtongmeng <tongmengwang@163.com>
2020-12-07 19:17:40 +0800

    模板编译：生成render函数2

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,6 +1,7 @@
 // ast语法树 是用对象来描述原生语法的
 // 虚拟dom 是用对象来描述dom节点的
 import {parseHTML} from './parser-html'
+const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g
 
 function genProps(attrs) { // 处理属性 拼接成属性的字符串
     // [{name: 'id',value: 'app'},...]
@@ -20,12 +21,46 @@ function genProps(attrs) { // 处理属性 拼接成属性的字符串
     }
     return `{${str.slice(0, -1)}}`
 }
+function genChildren(el) {
+    let children = el.children
+    if (children && children.length > 0) {
+        return `${children.map(c => gen(c)).join(',')}`
+    } else {
+        return false
+    }
+}
+function gen(node) {
+    if (node.type == 1) {
+        // 元素标签
+        return generate(node)
+    } else {
+        let text = node.text // a {{name}} b{{age}} c => _v("a"+_s(name)+"b"+_s(age)+"c")
+        let tokens = []
+        let match, index
+        let lastIndex = defaultTagRE.lastIndex = 0 // 只要是全局匹配 就需要将lastIndex每次匹配的时候调到0处
+        while (match = defaultTagRE.exec(text)) {
+            index = match.index
+            if (index > lastIndex) {
+                tokens.push(JSON.stringify(text.slice(lastIndex, index)))
+            }
+            tokens.push(`_s(${match[1].trim()})`)
+            lastIndex = index + match[0].length
+        }
+        if (lastIndex <text.length) {
+            tokens.push(JSON.stringify(text.slice(lastIndex)))
+        }
+        return `_v(${tokens.join('+')})`
+    }
+}
 function generate(el) {
+    let children = genChildren(el)
     let code = `_c("${el.tag}"),${
         el.attrs.length ? genProps(el.attrs) : 'undefined'
+    }${
+        children ? `,${children}` : ''
     }
 
-    `
+    `   
     return code
 }
 
@@ -44,7 +79,7 @@ export function compileToFunction(template) {
 
     // 所有的模板引擎实现 都需要new Function + with
     let renderFn = new Function(`with(this){ return ${code} }`)
+    console.log(renderFn)
     return renderFn
 }
-
 
